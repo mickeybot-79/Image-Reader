432e57c49815d93e664674a24bc557c4ca5a520d
mickeybot-79 <michaelperezvezoli@hotmail.com>
2025-06-12 16:31:53 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const app = express()
+const { pdfToPng } = require('pdf-to-png-converter')
+const Tesseract = require('tesseract.js')
+const PORT = process.env.PORT || 3500
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/member', (req, res) => {
+    const { content } = req.body
+    const testFunc = async () => {
+        let result = []
+        let numberOfPages = 0
+        const pngPages = await pdfToPng(content, {
+            disableFontFace: false,
+            useSystemFonts: false,
+            enableXfa: false,
+            viewportScale: 2.0,
+            outputFileMaskFunc: (pageNumber) => `page_${pageNumber}.png`,
+            strictPagesToProcess: false,
+            verbosityLevel: 0,
+        })
+
+        numberOfPages = pngPages.length
+
+        for (let i = 0; i < pngPages.length; i++) {
+
+            await Tesseract.recognize(
+                pngPages[i].content,
+                'eng',
+            )
+                .then(({ data: { text } }) => {
+                    result.push(text)
+                    if (result.length === numberOfPages) {
+                        const wholeString = result.toString()
+                        if (wholeString.search("UnitedHealthcare") !== -1) {
+                            if (wholeString.search("we've approved the service") !== -1) {
+                                const startIndex = wholeString.indexOf("Member name:")
+                                const nextLineBreak = wholeString.indexOf("\n", startIndex)
+                                const memberName = wholeString.substring(startIndex + 13, nextLineBreak)
+                                console.log('UnitedHealthcare member name:', memberName)
+                                res.send(memberName)
+                            }
+                        } else if (wholeString.search("Health and Human") !== -1) {
+                            if (wholeString.search("AUTHORIZATION FOR COMMUNITY CARE SERVICES") !== -1) {
+                                const startIndex = wholeString.indexOf("7. ")
+                                const nextLineBreak = wholeString.indexOf("\n", startIndex)
+                                const nextSpace = wholeString.indexOf(" ", nextLineBreak)
+                                const nextSpace2 = wholeString.indexOf(" ", nextSpace + 1)
+                                const memberName = wholeString.substring(nextLineBreak + 1, nextSpace2)
+                                console.log('HHSC member name:', memberName)
+                                res.send(memberName)
+                            }
+                        } else if (wholeString.search("Aetna") !== -1) {
+                            if (wholeString.search("SERVICE AUTHORIZATION") !== -1) {
+                                const startIndex = wholeString.indexOf("Member Name:")
+                                const nextLineBreak = wholeString.indexOf("\n", startIndex)
+                                const memberName = wholeString.substring(startIndex + 13, nextLineBreak)
+                                console.log('Aetna member name:', memberName)
+                                res.send(memberName)
+                            }
+                        } else if (wholeString.search("Molina Healthcare") !== -1) {
+                            if (wholeString.search("Authorization Approval Letter") !== -1 || wholeString.search("Authorization Notification") !== -1) {
+                                const startIndex = wholeString.indexOf("Member Name:")
+                                const nextLineBreak = wholeString.indexOf("\n", startIndex)
+                                const memberName = wholeString.substring(startIndex + 13, nextLineBreak)
+                                if (memberName.search("Requesting Provider") !== -1) {
+                                    const correctMemberName = memberName.substring(0, memberName.search("Requesting Provider"))
+                                    console.log('Molina member name:', correctMemberName)
+                                    res.send(correctMemberName)
+                                } else {
+                                    console.log('Molina member name:', memberName)
+                                    res.send(memberName)
+                                }
+                            }
+                        } else if (wholeString.search("Superior Health Plan") !== -1) {
+                            if (wholeString.search("Notification of Authorization") !== -1) {
+                                const startIndex = wholeString.indexOf("Member Name:")
+                                const nextLineBreak = wholeString.indexOf("\n", startIndex)
+                                const memberName = wholeString.substring(startIndex + 13, nextLineBreak)
+                                console.log('Superior member name:', memberName)
+                                res.send(memberName)
+                            }
+                        } else if (wholeString.search("Cook Childrens") !== -1) {
+                            if (wholeString.search("Authorization approval letters were sent") !== -1) {
+                                const startIndex = wholeString.indexOf("Case Name")
+                                const nextLineBreak = wholeString.indexOf("\n", startIndex)
+                                const nextSpace = wholeString.indexOf(" ", nextLineBreak)
+                                const nextSpace2 = wholeString.indexOf(" ", nextSpace + 1)
+                                const memberName = wholeString.substring(nextLineBreak + 1, nextSpace2)
+                                console.log('Cook member name:', memberName)
+                                res.send(memberName)
+                            }
+                        } else {
+                            console.log('no insurance found')
+                            res.send('no insurance found')
+                        }
+                    }
+                })
+                .catch(err => {
+                    console.error('Error during OCR:', err)
+                    res.send(err)
+                })
+        }
+    }
+
+    testFunc()
+
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
